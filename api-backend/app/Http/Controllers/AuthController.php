7e208aa420043495b89400eb11ef7bfba6c85891
Charlesbasis <charles.vh_14@live.com>
2025-09-28 23:42:46 +0600

    update: app/Http/Controllers/AuthController.php

---
+++ api-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // Register API (name, email, password, confirm_password)
+    public function register(){
+
+    }
+
+    // Login API (email, password)
+    public function login(){
+
+    }
+
+    
+}
