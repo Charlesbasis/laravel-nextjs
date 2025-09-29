0c2b1bb14d7315b4a1d43c44217a182d51057e9b
Charlesbasis <charles.vh_14@live.com>
2025-09-29 19:35:51 +0600

    fix: api-backend setup

--- api-backend/app/Http/Controllers/AuthController.php
+++ api-backend/app/Http/Controllers/AuthController.php
@@ -2,19 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     // Register API (name, email, password, confirm_password)
-    public function register(){
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users, email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
 
+        User::create($data);
+        return response()->json([
+            'status' => true,
+            'message' => 'User Registered successfully'
+        ]);
     }
 
     // Login API (email, password)
-    public function login(){
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid Credentials'
+            ]);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('myToken')->plainTextToken;
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User Logged in successfully',
+            'token' => $token
+        ]);
 
     }
 
-    
+    // Profile API
+    public function profile(){
+        $user = Auth::user();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User Profile Data',
+            'user' => $user
+        ]);
+    }
+
+    // Logout API
+    public function logout(){
+        Auth::logout();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User Logged out successfully'
+        ]);
+    }    
 }
--- api-backend/routes/api.php
+++ api-backend/routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group([
+    'middleware' => 'auth:sanctum',
+], function () {
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('products', ProductController::class);
+    
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
