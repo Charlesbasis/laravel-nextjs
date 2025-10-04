71451ea18aa7864b135b5267f886bdee010ac9c7
Charlesbasis <charles.vh_14@live.com>
2025-10-04 11:42:03 +0600

    fix: Register, Login

--- api-backend/app/Http/Controllers/AuthController.php
+++ api-backend/app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
     public function register(Request $request){
         $data = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users, email',
+            'email' => 'required|string|email|max:255|unique:users,email',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,27 +1,118 @@
+'use client';
+import { myAppHook } from "@/context/AppProvider";
+import { useState } from "react";
+
 export default function Auth() {
+
+    interface formData {
+        name?: string;
+        email: string;
+        password: string;
+        password_confirmation?: string;
+    }
+
+    const [isLogin, setIsLogin] = useState(false);
+    
+    const [formData, setFormData] = useState<formData>({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    });
+
+    const { login, register } = myAppHook();
+    
+    const handleOnChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        if (isLogin) {
+            
+            try {
+                await login(formData.email, formData.password);
+            } catch (error) {
+                console.log(`Authentication failed: ${error}`);
+            }
+        } else {
+            try {
+                await register( formData.name!, formData.email, formData.password, formData.password_confirmation! );
+            } catch (error) {
+                console.log(`Authentication failed: ${error}`);
+            }
+        }
+    };
+
     return (
         <>
             <div className="container d-flex justify-content-center align-items-center vh-100">
                 <div className="card p-4" style={{ width: '400px', marginTop: '50px' }}>
-                    <h3 className="text-center">Login</h3>
-                    <form>
-                        <input className="form-control mb-2" name="email" type="email" placeholder="Email" required />
-                        <input className="form-control mb-2" name="password" type="password" placeholder="Password" required />
-                        <button className="btn btn-primary w-100" type="submit">Login</button>
+                    <h3 className="text-center">{isLogin ? 'Login' : 'Register'}</h3>
+                    <form onSubmit={handleFormSubmit}>
+                        {!isLogin && (
+                            <input
+                                className="form-control mb-2"
+                                name="name"
+                                type="text"
+                                value={formData.name}
+                                onChange={handleOnChangeInput}
+                                placeholder="Name"
+                                required
+                            />
+                        )}
+
+                        <input
+                            className="form-control mb-2"
+                            name="email"
+                            type="email"
+                            value={formData.email}
+                            onChange={handleOnChangeInput}
+                            placeholder="Email"
+                            required
+                        />
+                        <input
+                            className="form-control mb-2"
+                            name="password"
+                            type="password"
+                            value={formData.password}
+                            onChange={handleOnChangeInput}
+                            placeholder="Password"
+                            required
+                        />
+
+                        {!isLogin && (
+                            <input
+                                className="form-control mb-2"
+                                name="password_confirmation"
+                                type="password"
+                                value={formData.password_confirmation}
+                                onChange={handleOnChangeInput}
+                                placeholder="Confirm Password"
+                                required
+                            />
+                        )}
+
+                        <button
+                            className="btn btn-primary w-100"
+                            type="submit"
+                        >
+                            {isLogin ? 'Login' : 'Register'}
+                        </button>
                     </form>
 
-                    <p className="mt-3 text-center">Don't have an account? Register</p>
+                    <p className="mt-3 text-center">
+                        {isLogin ? "Don't have an account? " : 'Already have an account? '}
+                        <span
+                            onClick={() => setIsLogin(!isLogin)}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            {isLogin ? 'Register' : 'Login'}
+                        </span>
+                    </p>
 
-                    <hr />
-
-                    <h3 className="text-center">Register</h3>
-                    <form>
-                        <input className="form-control mb-2" name="name" type="text" placeholder="Name" required />
-                        <input className="form-control mb-2" name="email" type="email" placeholder="Email" required />
-                        <input className="form-control mb-2" name="password" type="password" placeholder="Password" required />
-                        <input className="form-control mb-2" name="password_confirmation" type="password" placeholder="Confirm Password" required />
-                        <button className="btn btn-primary w-100" type="submit">Register</button>
-                    </form>
                 </div>
             </div>
         </>
